70dce5fccaaa001aa95090ee5167e3b2b9e3177a
Honminden <honminden@outlook.com>
2021-07-07 15:16:56 +0800

    add Spring210702

--- blog/src/assets/meta.js
+++ blog/src/assets/meta.js
@@ -1,4 +1,5 @@
 export {default as article0} from "raw-loader!@/assets/Hello.md"
+export {default as article1} from "raw-loader!@/assets/Spring210702/Spring210702.md"
 
 export {default as news90000} from "raw-loader!@/assets/news/FirstNews.md"
 
@@ -10,6 +11,14 @@ export const articles = [
         createTime: "2021.07.06",
         editTime: "2021.07.06",
         description: "a hello message"
+    },
+    {
+        id: "1",
+        title: "Spring项目搭建踩坑经验",
+        author: "Honminden",
+        createTime: "2021.07.07",
+        editTime: "2021.07.07",
+        description: "总结一下从头开始搭建一个Spring项目过程中踩的一些坑和其它经验。"
     }
 ]
 
